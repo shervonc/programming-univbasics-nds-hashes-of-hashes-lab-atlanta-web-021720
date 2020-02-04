12b10b79d1f72d7e5cc3fc49764814b3b6f639e4
Shervon Ceromain <shervonc@gmail.com>
2020-02-04 21:15:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,16 +46,16 @@ life = {
   sub_category: {  
               label: "Phylum",
               sub_category: { 
-                          label: "Class"},
+                          label: "Class",
                           sub_category:{
-                              label: "Order"},
+                              label: "Order",
                               sub_category: {
                                   label: "Family",
                                   sub_category: {
                                       label: "Genus",
                                       sub_category: {
                                           label: "Species",
-                                          sub_category: nil
+                                          sub_category: nil 
                                             }
                            
                                           }
